1cb33d46edec5eea61ca8697b46d7420c2cb4e02
王有为 <ywwang@kingdomai.com>
2025-08-02 17:03:15 +0800

    fix typo

--- download_resource.sh
+++ download_resource.sh
@@ -1,6 +1,6 @@
 # pip install modelscope
 # 下载模型
-modelscope download --model Qwen/Qwen2.5-Math-1.5B  --local_dir ./model/Qwen3-0.6B
+modelscope download --model Qwen/Qwen2.5-Math-1.5B  --local_dir ./model/Qwen2.5-Math-1.5B
 # modelscope download --model HuggingFaceTB/SmolVLM2-256M-Video-Instruct --local_dir ./model/SmolVLM2-256M-Video-Instruct
 
 # 下载数据集
